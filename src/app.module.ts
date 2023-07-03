48c6179b5c2f4ffd17e373a799ca3988e25a539e
khalilnazari <khalil.deve@gmail.com>
2023-07-03 13:00:55 +0800

    User services is updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+//
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,102 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private saldRound: number;
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {
+    this.saldRound = 10;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async create(newUser: CreateUserDto): Promise<User | { message: string }> {
+    const { email, password } = newUser;
+    newUser.password = await bcrypt.hash(password, this.saldRound);
+
+    try {
+      const userExist = await this.userRepository.findOneBy({
+        email,
+      });
+
+      if (userExist) {
+        throw new ConflictException('User exist');
+      }
+
+      const user = this.userRepository.create(newUser);
+      const response = await this.userRepository.save(user);
+
+      if (!response) {
+        throw new NotFoundException('Failed ');
+      }
+
+      return response;
+    } catch (error) {
+      return error.response;
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findAll() {
+    try {
+      const response = await this.userRepository.find();
+      if (!response) throw new NotFoundException();
+      return response;
+    } catch (error) {
+      return error.response;
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: number) {
+    try {
+      const user = await this.userRepository.findOneBy({ id });
+      if (!user) throw new NotFoundException();
+      const { password, ...rest } = user;
+      return user;
+    } catch (error) {
+      return error.response;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      const saltOrRounds = 10;
+      updateUserDto.password = await bcrypt.hash('random123', saltOrRounds);
+    }
+
+    try {
+      const user = await this.userRepository.findOneBy({ id });
+
+      if (!user) throw new NotFoundException();
+
+      Object.assign(user, updateUserDto);
+
+      const response = await this.userRepository.save(user);
+      const { password, ...rest } = response;
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  }
+
+  async remove(id: number) {
+    try {
+      const isExistUser = await this.userRepository.findOneBy({ id });
+      if (!isExistUser) throw new NotFoundException();
+      const user = await this.userRepository.remove(isExistUser);
+      return { message: `${user.email} has been deleted successfully` };
+    } catch (error) {
+      return error.response;
+    }
   }
 }
